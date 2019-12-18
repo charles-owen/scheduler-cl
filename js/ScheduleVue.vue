c980bbb59b2794fe81f63f382a6e26a5a1765fbf
cbowen <cbowen@cse.msu.edu>
2019-12-17 20:16:07 -0500

    Improvements in the scheduler interface Fresh build

--- js/ScheduleVue.vue
+++ js/ScheduleVue.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="content cl-schedule-view">
-    <div v-for="day of days"><h2>{{day.day}}</h2>
+    <div v-for="day of days"><h2>{{day.day}} <span class="cl-info">{{day.slots.length}} slots/{{day.slots.length - ownedForDay(day)}} open</span></h2>
       <table>
         <tr v-for="slot of day.slots" :class="slot.booked() ? (owned(slot) ? 'booked owned' : 'booked') : ''">
           <td>{{timeFormat(slot.time)}}<template v-if="staff"><br>
@@ -21,310 +21,320 @@
 </template>
 
 <script>
-	import Dialog from 'dialog-cl';
-	import {Member} from 'course-cl/js/Members/Member';
-	import {Schedule} from './Schedule';
-	import {Slot} from './Slot';
-	import UserVueBase from 'users-cl/js/Vue/UserVueBase.vue';
+  import Dialog from 'dialog-cl';
+  import {Member} from 'course-cl/js/Members/Member';
+  import {Schedule} from './Schedule';
+  import {Slot} from './Slot';
+  import UserVueBase from 'users-cl/js/Vue/UserVueBase.vue';
   import SlotEditorVue from './SlotEditor.vue';
   import {TimeFormatter} from 'site-cl/js/TimeFormatter';
   import {StickyNav} from 'site-cl/js/Util/StickyNav';
 
   export default {
-	  'extends': UserVueBase,
-	  props: ['json'],
-	  data: function () {
-		  return {
-			  schedule: null,
+    'extends': UserVueBase,
+    props: ['json'],
+    data: function () {
+      return {
+        schedule: null,
         days: [],
         teamMode: false,
         team: null,
-			  staff: false,
+        staff: false,
         latest: 0,
         time: 0,
         timer: 0
-		  }
-	  },
-	  mounted() {
-	    new StickyNav('nav.cl-nav');
-		  this.clearMenu();
-		  this.schedule = new Schedule(this.json.schedule);
-		  this.setTime();
+      }
+    },
+    mounted() {
+      new StickyNav('nav.cl-nav');
+      this.clearMenu();
+      this.schedule = new Schedule(this.json.schedule);
+      this.setTime();
 
-	    this.timer = setInterval(() => {
-		    this.setTime();
-	    }, 1000)
+      this.timer = setInterval(() => {
+        this.setTime();
+      }, 1000)
 
-		  this.newSlots(this.json.slots);
+      this.newSlots(this.json.slots);
 
-		  if(this.json.team !== undefined) {
-		  	this.teamMode = true;
-		  	this.team = this.json.team;
+      if (this.json.team !== undefined) {
+        this.teamMode = true;
+        this.team = this.json.team;
       }
 
-		  this.setTitle(this.schedule.name);
+      this.setTitle(this.schedule.name);
 
-		  this.staff = this.user.atLeast(Member.STAFF);
+      this.staff = this.user.atLeast(Member.STAFF);
 
-		  if (this.staff) {
-			  this.addMenu('Add Slot', () => {
-				  this.add();
-			  });
-		  }
+      if (this.staff) {
+        this.addMenu('Add Slot', () => {
+          this.add();
+        });
+      }
 
-	    this.$site.polling.addClient('scheduler', (params) => {
-	    	params.scheduler = {
-	    		scheduleId: this.schedule.id,
-	    		after: this.latest
-	    	};
-	    }, (response) => {
-	    	this.take(response);
-	    });
+      this.$site.polling.addClient('scheduler', (params) => {
+        params.scheduler = {
+          scheduleId: this.schedule.id,
+          after: this.latest
+        };
+      }, (response) => {
+        this.take(response);
+      });
 
-	  },
-    destroyed() {
-	  	clearInterval(this.timer);
-	  	this.timer = 0;
     },
-	  methods: {
-	  	take(response) {
+    destroyed() {
+      clearInterval(this.timer);
+      this.timer = 0;
+    },
+    methods: {
+      take(response) {
         const slots = response.getData('slots');
-        if(slots !== null) {
-	        const data = slots.attributes;
-	        this.newSlots(data);
+        if (slots !== null) {
+          const data = slots.attributes;
+          this.newSlots(data);
         }
-	    },
+      },
       owned(slot) {
-	  		if(this.teamMode) {
-	  			return this.team !== null && slot.teamId === this.team.id;
+        if (this.teamMode) {
+          return this.team !== null && slot.teamId === this.team.id;
         }
 
-	  		return this.user.member.id === slot.memberId;
+        return this.user.member.id === slot.memberId;
+      },
+      ownedForDay(day) {
+        let cnt = 0;
+        for(let slot of day.slots) {
+          if(slot.booked()) {
+            cnt++;
+          }
+        }
+
+        return cnt;
       },
       setTime() {
-	  		this.time = Math.round((new Date()).getTime() / 1000);
+        this.time = Math.round((new Date()).getTime() / 1000);
       },
       book(slot) {
-	  		let params = {id: slot.id, teamMode: this.teamMode ? 1 : 0};
-	  		if(this.teamMode) {
-		      if(this.team !== null) {
+        let params = {id: slot.id, teamMode: this.teamMode ? 1 : 0};
+        if (this.teamMode) {
+          if (this.team !== null) {
             params['teamId'] = this.team.id;
           } else {
-		        this.$site.toast(this, 'Not a member of a team!');
-		        return;
+            this.$site.toast(this, 'Not a member of a team!');
+            return;
           }
         }
 
 
-	      this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/book', params)
-		      .then((response) => {
-			      if (!response.hasError()) {
-				      this.take(response);
-			      } else {
-				      this.$site.toast(this, response);
-			      }
+        this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/book', params)
+                .then((response) => {
+                  if (!response.hasError()) {
+                    this.take(response);
+                  } else {
+                    this.$site.toast(this, response);
+                  }
 
-		      })
-		      .catch((error) => {
-			      this.$site.toast(this, error);
-		      });
+                })
+                .catch((error) => {
+                  this.$site.toast(this, error);
+                });
 
       },
       unbook(slot) {
-	      this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/unbook', {id: slot.id})
-		      .then((response) => {
-			      if (!response.hasError()) {
-				      this.take(response);
-			      } else {
-				      this.$site.toast(this, response);
-			      }
+        this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/unbook', {id: slot.id})
+                .then((response) => {
+                  if (!response.hasError()) {
+                    this.take(response);
+                  } else {
+                    this.$site.toast(this, response);
+                  }
 
-		      })
-		      .catch((error) => {
-			      this.$site.toast(this, error);
-		      });
+                })
+                .catch((error) => {
+                  this.$site.toast(this, error);
+                });
       },
       newSlots(slots) {
-	      //
-	      // Collect up slots by days
-	      //
-	      this.days = [];
+        //
+        // Collect up slots by days
+        //
+        this.days = [];
 
-	      let lastDay = null;
-	      let daySlots = [];
-	      for(let slotData of slots) {
-		      let slot = new Slot(slotData);
-		      if(slot.updated > this.latest) {
-		      	this.latest = slot.updated;
+        let lastDay = null;
+        let daySlots = [];
+        for (let slotData of slots) {
+          let slot = new Slot(slotData);
+          if (slot.updated > this.latest) {
+            this.latest = slot.updated;
           }
 
-		      let day = TimeFormatter.absoluteUNIX(slot.time, 'long-date');
-		      if(day !== lastDay) {
-			      // We have a new day
-			      if(daySlots.length > 0) {
-				      this.days.push({
-					      day: lastDay,
-					      slots: daySlots
-				      });
-			      }
-			      lastDay = day;
-			      daySlots = [];
-		      }
+          let day = TimeFormatter.absoluteUNIX(slot.time, 'long-date');
+          if (day !== lastDay) {
+            // We have a new day
+            if (daySlots.length > 0) {
+              this.days.push({
+                day: lastDay,
+                slots: daySlots
+              });
+            }
+            lastDay = day;
+            daySlots = [];
+          }
 
-		      daySlots.push(slot);
-	      }
+          daySlots.push(slot);
+        }
 
-	      if(daySlots.length > 0) {
-		      this.days.push({
-			      day: lastDay,
-			      slots: daySlots
-		      });
-	      }
+        if (daySlots.length > 0) {
+          this.days.push({
+            day: lastDay,
+            slots: daySlots
+          });
+        }
       },
-		  add() {
-			  const slot = new Slot();
+      add() {
+        const slot = new Slot();
 
-			  new Dialog({
-				  title: 'New Slot',
-				  content: '<div id="cl-schedule-slot"></div>',
+        new Dialog({
+          title: 'New Slot',
+          content: '<div id="cl-schedule-slot"></div>',
           addClass: 'cl-schedule-dialog',
-				  buttons: [
-					  {
-						  contents: 'Add',
-						  focus: true,
-						  click: (dialog) => {
-                if(slot.time === '' || slot.time === 0) {
+          buttons: [
+            {
+              contents: 'Add',
+              focus: true,
+              click: (dialog) => {
+                if (slot.time === '' || slot.time === 0) {
                   new Dialog.MessageBox('Must enter time', 'You must provide a date and time',
-                	  Dialog.MessageBox.OK);
-                  return;
-                }
-
-							  let params = {
-							    time: slot.time,
-                  duration: slot.duration,
-                  location: slot.location,
-                  repeat: slot.repeat
-							  }
-
-							  this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/new', params)
-							    .then((response) => {
-							  	  if (!response.hasError()) {
-							  		  this.take(response);
-							  		  dialog.close();
-							  	  } else {
-							  		  this.$site.toast(this, response);
-							  	  }
-
-							    })
-							    .catch((error) => {
-							  	  this.$site.toast(this, error);
-							    });
-
-						  }
-					  },
-					  {
-						  contents: 'Cancel',
-						  click: (dialog) => {
-							  dialog.close();
-						  }
-					  }
-				  ]
-			  });
-
-		    this.dialogVue(slot);
-		  },
-	    editer(slot) {
-		    slot = slot.clone();
-
-		    new Dialog({
-			    title: 'Edit Slot',
-		      content: '<div id="cl-schedule-slot"></div>',
-		      addClass: 'cl-schedule-dialog',
-			    buttons: [
-				    {
-					    contents: 'Update',
-					    focus: true,
-					    click: (dialog) => {
-                if(slot.time === '' || slot.time === 0) {
-                  new Dialog.MessageBox('Must enter time', 'You must provide a date and time',
-                    Dialog.MessageBox.OK);
+                          Dialog.MessageBox.OK);
                   return;
                 }
 
                 let params = {
-                	id: slot.id,
+                  time: slot.time,
+                  duration: slot.duration,
+                  location: slot.location,
+                  repeat: slot.repeat
+                }
+
+                this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/new', params)
+                        .then((response) => {
+                          if (!response.hasError()) {
+                            this.take(response);
+                            dialog.close();
+                          } else {
+                            this.$site.toast(this, response);
+                          }
+
+                        })
+                        .catch((error) => {
+                          this.$site.toast(this, error);
+                        });
+
+              }
+            },
+            {
+              contents: 'Cancel',
+              click: (dialog) => {
+                dialog.close();
+              }
+            }
+          ]
+        });
+
+        this.dialogVue(slot);
+      },
+      editer(slot) {
+        slot = slot.clone();
+
+        new Dialog({
+          title: 'Edit Slot',
+          content: '<div id="cl-schedule-slot"></div>',
+          addClass: 'cl-schedule-dialog',
+          buttons: [
+            {
+              contents: 'Update',
+              focus: true,
+              click: (dialog) => {
+                if (slot.time === '' || slot.time === 0) {
+                  new Dialog.MessageBox('Must enter time', 'You must provide a date and time',
+                          Dialog.MessageBox.OK);
+                  return;
+                }
+
+                let params = {
+                  id: slot.id,
                   time: slot.time,
                   duration: slot.duration,
                   location: slot.location
                 }
 
                 this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/update', params)
-                  .then((response) => {
-                    if (!response.hasError()) {
-                      this.take(response);
-                      dialog.close();
-                    } else {
-                      this.$site.toast(this, response);
-                    }
+                        .then((response) => {
+                          if (!response.hasError()) {
+                            this.take(response);
+                            dialog.close();
+                          } else {
+                            this.$site.toast(this, response);
+                          }
 
-                  })
-                  .catch((error) => {
-                    this.$site.toast(this, error);
-                  });
+                        })
+                        .catch((error) => {
+                          this.$site.toast(this, error);
+                        });
 
-					    }
-				    },
-				    {
-					    contents: 'Cancel',
-					    click: (dialog) => {
-						    dialog.close();
-					    }
-				    }
-			    ]
+              }
+            },
+            {
+              contents: 'Cancel',
+              click: (dialog) => {
+                dialog.close();
+              }
+            }
+          ]
 
-		    });
+        });
 
-		    this.dialogVue(slot);
-	    },
+        this.dialogVue(slot);
+      },
       dialogVue(slot) {
-		    // Create a Vue inside the dialog box
+        // Create a Vue inside the dialog box
 
-		    new this.$site.Vue({
-			    el: '#cl-schedule-slot',
+        new this.$site.Vue({
+          el: '#cl-schedule-slot',
           site: this.$site,
-			    data: function() {
-				    return {
-			        slot: slot
-				    }
-			    },
-			    template: `<editor :schedule-slot="slot"></editor>`,
-			    components: {
-				    editor: SlotEditorVue
-			    }
-		    })
-	    },
-	    deleter(slot) {
-		    new Dialog.MessageBox('Are you sure?', 'Are you sure you want to delete?',
-			    Dialog.MessageBox.OKCANCEL, () => {
-				    this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/delete', {id: slot.id})
-					    .then((response) => {
-						    if (!response.hasError()) {
-							    this.take(response);
-							    dialog.close();
-						    } else {
-							    this.$site.toast(this, response);
-						    }
+          data: function () {
+            return {
+              slot: slot
+            }
+          },
+          template: `<editor :schedule-slot="slot"></editor>`,
+          components: {
+            editor: SlotEditorVue
+          }
+        })
+      },
+      deleter(slot) {
+        new Dialog.MessageBox('Are you sure?', 'Are you sure you want to delete?',
+                Dialog.MessageBox.OKCANCEL, () => {
+                  this.$site.api.post('/api/scheduler/slots/' + this.schedule.id + '/delete', {id: slot.id})
+                          .then((response) => {
+                            if (!response.hasError()) {
+                              this.take(response);
+                              dialog.close();
+                            } else {
+                              this.$site.toast(this, response);
+                            }
 
-					    })
-					    .catch((error) => {
-						    this.$site.toast(this, error);
-					    });
-			    });
-	    },
+                          })
+                          .catch((error) => {
+                            this.$site.toast(this, error);
+                          });
+                });
+      },
       timeFormat(time) {
-	  		return TimeFormatter.absoluteUNIX(time, 'short-time');
+        return TimeFormatter.absoluteUNIX(time, 'short-time');
       }
-	  }
+    }
   }
 </script>
 
@@ -339,6 +349,11 @@
       border: 0;
       text-align: center;
       padding-left: 3.5em;
+    }
+
+    span.cl-info {
+      font-weight: normal;
+      font-size: 0.7em;
     }
 
     table {
--- js/SlotEditor.vue
+++ js/SlotEditor.vue
@@ -4,8 +4,7 @@
       <form>
         <p class="flatpickr">
           <label>Date and Time<br>
-          <flat-pickr v-model="time" :config="fpConfig"
-                      @on-change="change($event)" data-input></flat-pickr>
+          <flat-pickr v-model="time" :config="fpConfig" data-input></flat-pickr>
           <a data-toggle title="Toggle Calendar"><span class="icons-cl icons-cl-calendar"></span></a>
           </label>
         </p>
@@ -18,32 +17,45 @@
 </template>
 
 <script>
-	import flatPickr from 'vue-flatpickr-component';
-	import 'flatpickr/dist/flatpickr.css';
+  import flatPickr from 'vue-flatpickr-component';
+  import 'flatpickr/dist/flatpickr.css';
 
   export default {
-	  props: ['schedule-slot'],
-	  data: function () {
-		  return {
+    props: ['schedule-slot'],
+    data: function () {
+      return {
         time: null,
-			  fpConfig: {
-				  enableTime: true,
-				  dateFormat: "m-d-Y h:iK",
-				  wrap: true
-			  }
-		  }
-	  },
-	  components: {
-		  'flatPickr': flatPickr
-	  },
-	  mounted() {
-	  	this.time = this.scheduleSlot.time !== 0 ? this.scheduleSlot.time * 1000 : null;
-	  },
-    methods: {
-      change(event, user) {
-        let str = event[0];
-        this.scheduleSlot.time = str !== '' ? Math.floor((new Date(str)).getTime() / 1000) : 0;
+        fpConfig: {
+          enableTime: true,
+          dateFormat: "m-d-Y h:iK",
+          wrap: true
+        }
       }
+    },
+    components: {
+      'flatPickr': flatPickr
+    },
+    mounted() {
+      this.time = this.scheduleSlot.time !== 0 ? this.scheduleSlot.time * 1000 : null;
+    },
+    updated() {
+      if (this.time === null || (typeof this.time) === 'number') {
+        return;
+      }
+
+      const matches = this.time.match(/([0-9]*)-([0-9]*)-([0-9]*)\s([0-9]*):([0-9]*)([AP])/);
+      let hours = +matches[4];
+      if (matches[6] === 'P') {
+        if (hours < 12) {
+          hours += 12;
+        }
+      } else {
+        if (hours === 12) {
+          hours = 0;
+        }
+      }
+      const date = new Date(matches[3], matches[1] - 1, matches[2], hours, matches[5]);
+      this.scheduleSlot.time = this.time !== '' ? Math.floor(date.getTime() / 1000) : 0;
     }
   }
 </script>
